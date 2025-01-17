b386ddcbca6cafabb4f531be89f798afbb8da35b
Corgeek <tkg.develop@gmail.com>
2025-01-17 20:18:26 +0900

    add runner option generator for linux

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,17 +3,27 @@
 BOARD_TYPE=rpi_pico
 BOARD_TYPE=nucleo_f030r8
 BOARD_TYPE=nucleo_f401re
+BOARD_TYPE=nucleo_l552ze_q
 BOARD_TYPE=bbc_microbit
 BOARD_TYPE=bbc_microbit_v2
 
 SCRIPT_PATH=`readlink -f ${0}`
-pushd `dirname ${SCRIPT_PATH}`/.. > /dev/null
+SCRIPT_DIR=`dirname ${SCRIPT_PATH}`
+
+pushd ${SCRIPT_DIR} > /dev/null
+RUNNER_FLASH=`python3 opt_gen.py flash ${BOARD_TYPE}`
+RUNNER_DEBUG=`python3 opt_gen.py debug ${BOARD_TYPE}`
+popd > /dev/null
+
+pushd ${SCRIPT_DIR}/.. > /dev/null
 PROJ_PATH=`readlink -f .`
 ZEPHYR_ROOT=`readlink -f ..`
 
 cat > scripts/west_env.bat << EOF
 ZEPHYR_ROOT=${ZEPHYR_ROOT}
 BOARD_TYPE=${BOARD_TYPE}
+RUNNER_FLASH="${RUNNER_FLASH}"
+RUNNER_DEBUG="${RUNNER_DEBUG}"
 
 if [ ! -e \${ZEPHYR_ROOT}/zephyr/zephyr-env.sh ]; then
 	echo "zephyr-env.sh is not found"
