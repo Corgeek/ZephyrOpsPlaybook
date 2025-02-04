b464d458ec56bb8eb5f8a023d8ec6844267663a3
Corgeek <tkg.develop@gmail.com>
2025-02-04 13:07:11 +0900

    dev/upd scripts (#1)
    * add stop.bat gimmick
    
    * preparing to switching from bash to python
    
    * fix stop.bat
    
    * fix setup.bat
    
    * add setup scripts for dos and linux
    
    * update README_xxx.md
    
    * add setup.bat / setup.sh file to ignorelist

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,43 +12,5 @@ SCRIPT_PATH=`readlink -f ${0}`
 SCRIPT_DIR=`dirname ${SCRIPT_PATH}`
 
 pushd ${SCRIPT_DIR} > /dev/null
-RUNNER_FLASH=`python3 opt_gen.py flash ${BOARD_TYPE}`
-RUNNER_DEBUG=`python3 opt_gen.py debug ${BOARD_TYPE}`
-popd > /dev/null
-
-pushd ${SCRIPT_DIR}/.. > /dev/null
-PROJ_PATH=`readlink -f .`
-ZEPHYR_ROOT=`readlink -f ..`
-
-cat > scripts/west_env.bat << EOF
-ZEPHYR_ROOT=${ZEPHYR_ROOT}
-BOARD_TYPE=${BOARD_TYPE}
-RUNNER_FLASH="${RUNNER_FLASH}"
-RUNNER_DEBUG="${RUNNER_DEBUG}"
-
-if [ ! -e \${ZEPHYR_ROOT}/zephyr/zephyr-env.sh ]; then
-	echo "zephyr-env.sh is not found"
-	exit 1
-fi
-source \${ZEPHYR_ROOT}/zephyr/zephyr-env.sh
-
-if [ -e \${ZEPHYR_ROOT}/.venv/bin/activate ]; then
-	source \${ZEPHYR_ROOT}/.venv/bin/activate
-fi
-EOF
-
-SDK_LIST=(`ls -r ${HOME} | grep zephyr-sdk-`)
-cat > .vscode/settings.json << EOF
-{
-  "ZEPHYRSDK": "${HOME}/${SDK_LIST[0]}",
-  "PROJ_PATH": "${PROJ_PATH}",
-  "CROSS_GDB_PATH": "\${config:ZEPHYRSDK}/arm-zephyr-eabi/bin/arm-zephyr-eabi-gdb"
-}
-EOF
-
-mkdir -p ${ZEPHYR_ROOT}/.vscode
-cp -f ${PROJ_PATH}/.vscode/*.json ${ZEPHYR_ROOT}/.vscode
-cp -f scripts/linux/build.sh scripts/build.bat
-cp -f scripts/linux/debug.sh scripts/debug.bat
-
+python3 setup.py ${BOARD_TYPE}
 popd > /dev/null
