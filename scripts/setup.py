e5f673d7dbb4bf2493585818151808af17ad468d
Corgeek <tkg.develop@gmail.com>
2025-02-07 13:02:42 +0900

    when zephyr v3.7.0, use 0.16.8 sdk

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,6 +2,7 @@
 
 import sys
 import platform
+import yaml
 import json
 from pathlib import Path
 import re
@@ -14,26 +15,40 @@ else:
 
 BOARD_TYPE = ""
 
-def setup_env() -> tuple[str, str, str]:
+def get_base_paths() -> tuple[str, str, str]:
     scripts_dir = Path(__file__).resolve().parent
     proj_dir = scripts_dir.parent
     zephyr_root = proj_dir.parent
 
+    return zephyr_root, proj_dir, scripts_dir
+
+def get_zephyr_base_version(proj_dir: str) -> str:
+    zephyr_ver = ""
+    west_path = Path(proj_dir) / "west.yml"
+    with open(west_path) as west_file:
+        west_yml = yaml.safe_load(west_file)
+        zephyr_ver = west_yml.get("manifest", {}).get("projects", [])[0].get("revision", {})
+
+    return zephyr_ver
+
+def set_west_env_file(zephyr_root: str, proj_dir: str, scripts_dir: str, sdk_path: str) -> tuple[str, str, str]:
     runner_flash = runner.get_runner(cmd_type="flash", board_type=BOARD_TYPE)
     runner_debug = runner.get_runner(cmd_type="debug", board_type=BOARD_TYPE)
 
     west_env_path = scripts_dir / "west_env.bat"
 
     env_content = os_dep.gen_env_content(zephyr_root=zephyr_root,
-                               BOARD_TYPE=BOARD_TYPE,
-                               runner_flash=runner_flash,
-                               runner_debug=runner_debug)
+                                         BOARD_TYPE=BOARD_TYPE,
+                                         runner_flash=runner_flash,
+                                         runner_debug=runner_debug,
+                                         sdk_path=sdk_path
+                                         )
 
     west_env_path.write_text(env_content, encoding="utf-8")
 
     return zephyr_root, proj_dir, scripts_dir
 
-def get_sdk_paths():
+def get_sdk_paths(zephyr_ver: str) -> str:
     found_sdks = {}
     sdk_pattern = re.compile(r"zephyr-sdk-(\d+\.\d+\.\d+)")
 
@@ -53,9 +68,21 @@ def get_sdk_paths():
     if not sorted_sdks:
         raise FileNotFoundError("No Zephyr SDK found in specified directories.")
 
-    return sorted_sdks
+    print("Installed SDK:")
+    for sdk_path in sorted_sdks:
+        print("\t" + str(sdk_path[0]) + " : " + str(sdk_path[1]))
 
-def update_settings(proj_dir: str, selected_sdk: str):
+    if zephyr_ver == "v3.7.0":
+        if "zephyr-sdk-0.16.8" in found_sdks:
+            print("USE:\r\n\t" + str(found_sdks["zephyr-sdk-0.16.8"].as_posix()))
+            selected_sdk = found_sdks["zephyr-sdk-0.16.8"]
+    else:
+        print("USE:\r\n\t" + str(sorted_sdks[0][1]))
+        selected_sdk = sorted_sdks[0][1]
+
+    return selected_sdk
+
+def update_settings(proj_dir: str, selected_sdk: str) -> None:
     vscode_dir = Path(proj_dir) / ".vscode"
     settings_file = vscode_dir / "settings.json"
 
@@ -88,15 +115,10 @@ if __name__ == "__main__":
     BOARD_TYPE = sys.argv[1]
 
     print ("BOARD_TYPE:\r\n\t" + str(BOARD_TYPE) + "\r\n")
-    zephyr_root, proj_dir, scripts_dir = setup_env()
+    zephyr_root, proj_dir, scripts_dir = get_base_paths()
+    zephyr_ver = get_zephyr_base_version(proj_dir=proj_dir)
+    selected_sdk = get_sdk_paths(zephyr_ver)
 
-    sorted_sdks = get_sdk_paths()
-    selected_sdk = sorted_sdks[0][1]
-
-    print("Installed SDK:")
-    for sdk_path in sorted_sdks:
-        print("\t" + str(sdk_path[0]) + " : " + str(sdk_path[1]))
-    print("USE:\r\n\t" + str(selected_sdk))
-
+    set_west_env_file(zephyr_root=zephyr_root, proj_dir=proj_dir, scripts_dir=scripts_dir, sdk_path=selected_sdk)
     update_settings(proj_dir=proj_dir, selected_sdk=selected_sdk)
     os_dep.duplicate_scripts(zephyr_root=zephyr_root, proj_dir=proj_dir, scripts_dir=scripts_dir)
