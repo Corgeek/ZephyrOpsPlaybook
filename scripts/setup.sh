ac4a124ae13cb132ec6bde73d3c928c3e8dc80c6
Corgeek <tkg.develop@gmail.com>
2025-03-08 08:59:46 +0900

    exhibited at OSC 2025 Tokyo/Spring

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-BOARD_TYPE=rpi_pico
-BOARD_TYPE=rpi_pico2/rp2350a/m33
-BOARD_TYPE=nucleo_f030r8
-BOARD_TYPE=nucleo_f401re
-BOARD_TYPE=nucleo_l552ze_q
 BOARD_TYPE=stm32h747i_disco/stm32h747xx/m7
-BOARD_TYPE=bbc_microbit
-BOARD_TYPE=bbc_microbit_v2
 
 SCRIPT_PATH=`readlink -f ${0}`
 SCRIPT_DIR=`dirname ${SCRIPT_PATH}`
--- src/main.c
+++ src/main.c
@@ -5,12 +5,22 @@
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/display.h>
+#include <lvgl.h>
 #include "boards/unique.h"
+
+extern const lv_image_dsc_t g_img_osc_tokyo;
 
 int main(void)
 {
+	lv_obj_t *img;
+
+	img = lv_image_create(lv_screen_active());
+	lv_image_set_src(img, &g_img_osc_tokyo);
+	lv_obj_center(img);
+
 	while (true) {
-		k_msleep(1000);
+		k_msleep(MIN(lv_timer_handler(), INT32_MAX));
 	}
 
 	return 0;
