fad174bfec80a41dc5576f54f0e095901e64bf7c
Corgeek <tkg.develop@gmail.com>
2025-12-14 15:05:42 +0900

    unify drivers interface for generic

--- include/boards/generic/generic_board.h
+++ include/boards/generic/generic_board.h
@@ -4,12 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #pragma once
+#include "drivers/serial/drv_uart_common.h"
+#include "drivers/i2c/drv_i2c_common.h"
 
 #ifdef  __cplusplus
 extern "C" {
 #endif
 
 // aliases
+#define HOST_UART                   DEVICE_DT_GET(DT_NODELABEL(uart0))
+#define I2C_100KHZ_BUS              DEVICE_DT_GET(DT_NODELABEL(i2c0))
 #define USER_LED                    GPIO_DUMMY
 
 #ifdef  __cplusplus
