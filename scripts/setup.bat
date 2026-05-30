8820f44001555f6b8251ec03c2ba53e492197d5f
Corgeek <tkg.develop@gmail.com>
2026-05-31 00:52:27 +0900

    adjust scripts

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -5,7 +5,6 @@ set BOARD_TYPE=nucleo_g431kb
 set BOARD_TYPE=nucleo_f030r8
 set BOARD_TYPE=nucleo_f401re
 set BOARD_TYPE=nucleo_l552ze_q
-set BOARD_TYPE=stm32h747i_disco/stm32h747xx/m7
 set BOARD_TYPE=arduino_uno_q
 
 set SCRIPT_DIR=%~dp0
