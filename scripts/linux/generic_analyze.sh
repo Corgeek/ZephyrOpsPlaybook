54a94774075e269226b33a071904051321daf8aa
Corgeek <tkg.develop@gmail.com>
2026-07-29 02:20:59 +0900

    add options for codechecker

--- scripts/linux/generic_analyze.sh
+++ scripts/linux/generic_analyze.sh
@@ -6,8 +6,13 @@ PROJ_PATH=`readlink -f .`
 
 source scripts/west_env.bat
 
-SCA_OPT="-DZEPHYR_SCA_VARIANT=codechecker -DCODECHECKER_EXPORT=html"
+SCA_OPT="-DZEPHYR_SCA_VARIANT=codechecker -DCODECHECKER_EXPORT=html -DCODECHECKER_PARSE_EXIT_STATUS=y"
 SCA_DIR=build/sca/codechecker
+
+CC_CONFIG=${ZEPHYR_BASE}/.codechecker.yml
+if [ -f ${CC_CONFIG} ]; then
+	SCA_OPT="${SCA_OPT} -DCODECHECKER_CONFIG_FILE=${CC_CONFIG} -DCODECHECKER_ANALYZE_OPTS=--no-missing-checker-error"
+fi
 
 if [ "/r" = "${1}" ]; then
 	west build -p -b ${BOARD_TYPE} -- ${SCA_OPT}
@@ -16,7 +21,7 @@ else
 fi
 RET=$?
 
-if [ 0 -eq ${RET} ] && [ -d ${SCA_DIR}/codechecker.plist ]; then
+if [ -d ${SCA_DIR}/codechecker.plist ]; then
 	ln -sfn codechecker.plist ${SCA_DIR}/reports
 fi
 
